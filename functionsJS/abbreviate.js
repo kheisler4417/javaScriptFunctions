571c40a77d018daa87e8b6fd2ef79ada627a57e4
kheisler4417 <kheisler4417@gmail.com>
2023-02-25 10:24:25 -0500

    added a semicolon and updated git creds in PwrShll

--- functionsJS/abbreviate.js
+++ functionsJS/abbreviate.js
@@ -1,12 +1,12 @@
 function abbreviate(str) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    for (let i = 0; i < str.length; i++) {
-      if (vowels.includes(str[i])) {
-            const before = str.slice(0, i);
-            const after = str.slice(i + 1);
-            str = before + after
-            i = -1
-      }
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      const before = str.slice(0, i);
+      const after = str.slice(i + 1);
+      str = before + after;
+      i = -1
     }
-    return str
   }
+  return str
+}

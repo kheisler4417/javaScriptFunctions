5d4afd63ddf701a11f772ac07b59a4520d66bcc4
kheisler4417 <kheisler4417@gmail.com>
2023-03-01 20:39:30 -0500

    question head updates

--- functionsJS/pascalsTriangle.js
+++ functionsJS/pascalsTriangle.js
@@ -1,3 +1,13 @@
+// Pascal's triangle is a 2-dimensional array with the shape of a pyramid.
+// The top of the pyramid is the number 1. To generate further levels of the
+// pyramid, every element is the sum of the element above and to the left with
+// the element above and to the right.Non - existing elements are treated as 0
+// when calculating the sum.
+
+// Write a function pascalsTriangle that accepts a positive number, n, as an
+// argument and returns a 2 - dimensional array representing the first n levels
+// of Pascal's triangle. See the file for examples.
+
 // For example, here are the first 5 levels of Pascal's triangle:
 //       1
 //      1 1
